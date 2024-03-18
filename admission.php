331e8844d9053c8a35c2dc363bdf0a7c5a9a6f0d
Louise Ruzzele H. Ponciano <lrhponciano@SSCGI.COM>
2024-03-18 08:59:23 +0800

    added information for admission requirements

--- admission.php
+++ admission.php
@@ -114,7 +114,11 @@
           </div>
           <div id="collapseOne" class="collapse show" data-parent="#accordion">
             <div class="card-body">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            <ul>
+              <li>F138 (CARD)</li>
+              <li>Birth Certificate Xerox</li>
+              <li>Good moral Certificate</li>
+            </ul>
             </div>
           </div>
         </div>
