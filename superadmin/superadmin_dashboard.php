da8eeae22dfc18793fde662c72d2441d70642ef8
unknown <louiseruzzelep@gmail.com>
2024-03-23 10:59:19 +0800

    v1.1 phase 1

--- superadmin/superadmin_dashboard.php
+++ superadmin/superadmin_dashboard.php
@@ -39,9 +39,72 @@ if(!isset($superadmin_id)){
   <link href="../assets/vendor/quill/quill.bubble.css" rel="stylesheet">
   <link href="../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
   <link href="../assets/vendor/simple-datatables/style.css" rel="stylesheet">
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
 
   <!-- Template Main CSS File -->
   <link href="../assets/css/style.css" rel="stylesheet">
+  <style>
+    .card-counter{
+    box-shadow: 2px 2px 10px #DADADA;
+    margin: 5px;
+    padding: 20px 10px;
+    background-color: #fff;
+    height: 100px;
+    border-radius: 5px;
+    transition: .3s linear all;
+  }
+
+  .card-counter:hover{
+    box-shadow: 4px 4px 20px #DADADA;
+    transition: .3s linear all;
+  }
+
+  .card-counter.primary{
+    background-color: #007bff;
+    color: #FFF;
+  }
+
+  .card-counter.danger{
+    background-color: #ef5350;
+    color: #FFF;
+  }  
+
+  .card-counter.success{
+    background-color: #66bb6a;
+    color: #FFF;
+  }  
+
+  .card-counter.info{
+    background-color: #26c6da;
+    color: #FFF;
+  }  
+
+  .card-counter i{
+    font-size: 5em;
+    opacity: 0.2;
+  }
+
+  .card-counter .count-numbers{
+    position: absolute;
+    right: 35px;
+    top: 20px;
+    font-size: 32px;
+    display: block;
+  }
+
+  .card-counter .count-name{
+    position: absolute;
+    right: 35px;
+    top: 65px;
+    font-style: italic;
+    text-transform: capitalize;
+    opacity: 0.5;
+    display: block;
+    font-size: 18px;
+  }
+  </style>
 </head>
 
 <body>
@@ -63,9 +126,41 @@ if(!isset($superadmin_id)){
 </div><!-- End Page Title -->
 <div class="row">
 <div class="col-lg-12">
+<div class="container">
+    <div class="row">
+    <div class="col-md-3">
+      <div class="card-counter primary">
+        <i class="fa fa-user"></i>
+        <span class="count-numbers">12</span>
+        <span class="count-name">Super Admin</span>
+      </div>
+    </div>
 
+    <div class="col-md-3">
+      <div class="card-counter danger">
+        <i class="fa fa-user"></i>
+        <span class="count-numbers">599</span>
+        <span class="count-name">Registrar</span>
+      </div>
+    </div>
 
+    <div class="col-md-3">
+      <div class="card-counter success">
+        <i class="fa fa-user"></i>
+        <span class="count-numbers">6875</span>
+        <span class="count-name">Accounting</span>
+      </div>
+    </div>
 
+    <div class="col-md-3">
+      <div class="card-counter info">
+        <i class="fa fa-users"></i>
+        <span class="count-numbers">35</span>
+        <span class="count-name">Teacher</span>
+      </div>
+    </div>
+  </div>
+</div>
 </div>
 </div>
 </div>
