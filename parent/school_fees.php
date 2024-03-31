92ba7866bab6b82878664f9a9602dfd6b5816e78
unknown <louiseruzzelep@gmail.com>
2024-03-31 23:31:50 +0800

    modal on payment

--- parent/school_fees.php
+++ parent/school_fees.php
@@ -47,27 +47,9 @@ if(!isset($parent_id)){
         </div><!-- End Page Title -->
 
         <section class="section">
-        <div class="row justify-content-center">
-    <div class="col-lg-4">
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title text-center">Installment</h5>
-                
-            </div>
-        </div>
-    </div>
-    <div class="col-lg-4">
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title text-center">Cash Basis</h5>
-                
-            </div>
-        </div>
-    </div>
-</div>
 
             <div class="row">
-                <div class="col-lg-12">
+                <div class="col-lg-8">
 
                     <div class="card">
                         <div class="card-body">
@@ -100,7 +82,8 @@ if(!isset($parent_id)){
                                         echo "<td>" .$row['grade_level']."</td>";
                                         echo "<td>" . $row['total_whole_year'] . "</td>";
                                         echo "<td>";
-                                            
+                                            // Button trigger modal
+                                            echo '';
                                         echo "</td>";
                                     echo "</tr>";
                                 }
@@ -115,10 +98,45 @@ if(!isset($parent_id)){
                         echo "Oops! Something went wrong. Please try again later.";
                     }
  
-                    // Close connection
-                    mysqli_close($link);
+                 
                     ?>
 
+                    </div>
+                </div>
+
+                <div class="col-lg-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <div style="text-align: center;">
+                            <?php
+// Include config file
+require_once "config1.php";
+
+// Attempt select query execution
+$sql = "SELECT *
+        FROM student
+        INNER JOIN payments on student.grade_level = payments.grade_level 
+        WHERE student.userId = $parent_id";
+if($result = mysqli_query($link, $sql)){
+    if(mysqli_num_rows($result) > 0){
+        while ($row = mysqli_fetch_array($result)) {
+            echo ' <h5 class="card-title">Balance: ₱'.$row['total_whole_year'].'</h5>';
+        }
+        // Free result set
+        mysqli_free_result($result);
+    } else{
+        echo '<div class="alert alert-danger"><em>No records were found.</em></div>';
+    }
+} else{
+    echo "Oops! Something went wrong. Please try again later.";
+}
+// Close connection
+mysqli_close($link);
+?>
+
+                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#paymentModal">Make a Payment</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
@@ -128,7 +146,28 @@ if(!isset($parent_id)){
 
     </main><!-- End #main -->
 
-
+    <!-- Modal -->
+    <div class="modal fade" id="paymentModal" tabindex="-1" aria-labelledby="paymentModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="paymentModalLabel">Make a Payment</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                
+                <div class="modal-body">
+                    <!-- Add payment form here -->
+                    <form method="post" action="make_payment.php">
+                        <!-- Payment form fields go here -->
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <?php
     include 'footer.php';
