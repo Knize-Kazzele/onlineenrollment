1a8ba8425959905e5a9dc45becadb5e3b11ecb3d
unknown <louiseruzzelep@gmail.com>
2024-03-31 19:31:19 +0800

    ui update v1

--- superadmin/sidebar.php
+++ superadmin/sidebar.php
@@ -1,5 +1,5 @@
 <!-- ======= Sidebar ======= -->
-<aside id="sidebar" class="sidebar" style="background-color: #373746;">
+<aside id="sidebar" class="sidebar">
 
 <ul class="sidebar-nav" id="sidebar-nav">
 
